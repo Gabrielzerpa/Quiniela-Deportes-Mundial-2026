6deb781efe732ebf559c67cd1965a8d122cf679a
Gabrielzerpa <gazacarigua@gmail.com>
2026-05-07 18:09:30 -0500

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -4,7 +4,20 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
+  const token_hash = searchParams.get("token_hash");
+  const type = searchParams.get("type");
   const next = searchParams.get("next") ?? "/";
+
+  if (token_hash && type) {
+    const supabase = createClient();
+    const { error } = await supabase.auth.verifyOtp({
+      type: type as "email" | "recovery" | "invite" | "email_change" | "magiclink",
+      token_hash,
+    });
+    if (!error) {
+      return NextResponse.redirect(`${origin}${next}`);
+    }
+  }
 
   if (code) {
     const supabase = createClient();
